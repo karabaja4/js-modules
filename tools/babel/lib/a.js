068e1a7c65baa1942f7159e444ae0bd8a49e56e8
Igor Saric <igor.saric@outlook.com>
2020-03-13 09:39:18 +0100

    a

---
+++ tools/babel/lib/a.js
@@ -0,0 +1,10 @@
+class A {
+  constructor(name) {
+    this.name = name;
+  }
+  print() {
+    console.log('hello from a');
+  }
+};
+
+module.exports = A;
--- tools/babel/test.js
+++ tools/babel/test.js
@@ -1,6 +1,10 @@
+import { A } from './lib/a';
+
 class Test {
   constructor(name) {
     console.log(`I am a Test class... ${name}`);
+    const a = new A();
+    a.print();
   }
 }
 
